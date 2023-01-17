f81a38ac272b4372391790e4a738be88eddc6683
spital <spital@seznam.cz>
2023-01-17 22:58:16 +0100

    v6.1.6-200.fc37 builds OK

--- build.sh
+++ build.sh
@@ -16,13 +16,16 @@ MBP_VERSION=mbp
 
 
 export fedver=fc37
+export relver=6.1.6
+export relrel=200
 #FEDORA_KERNEL_VERSION=6.1.0-65.$fedver
-FEDORA_KERNEL_VERSION=6.1.5-200.$fedver  # target
+FEDORA_KERNEL_VERSION=$relver-$relrel.$fedver  # target
 #FEDORA_KERNEL_VERSION_SRC=6.1.0-65.fc38
+#FEDORA_KERNEL_VERSION_SRC=6.2.0-0.rc2.18.fc38 # source
 FEDORA_KERNEL_VERSION_SRC=$FEDORA_KERNEL_VERSION  # source
 # no '-' in release !
 F=./yum-repo/mbp-fedora-t2-config/rpm.spec
-sed -e 's/Version: 6.1.2/Version: 6.1.5/g' $F.src | sed -e 's/Release: 200%/Release: 200%/g' > $F
+sed -e "s/Version: 6.1.2/Version: $relver/g" $F.src | sed -e "s/Release: 200%/Release: $relrel%/g" > $F
 # TODO add rustc 1.62 and bindgen 0.56 https://lwn.net/Articles/910762/
 RUST_VER="1.62.0"
 BINDGEN_VER="0.56.0"
